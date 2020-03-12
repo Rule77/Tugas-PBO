4c15a61f7393e19abf099f9264dc0935cbfecdc5
Achrul Abdullah <57305420+Rule77@users.noreply.github.com>
2020-03-12 22:11:09 +0700

    Add files via upload

---
+++ PBO2.java
@@ -0,0 +1,5 @@
+public class PBO2{
+	public static void main (String[] args){
+		System.out.println("Belajar Bahasa Pemrograman Java");
+	}
+}
---
+++ PBO3.java
@@ -0,0 +1,5 @@
+public class PBO3{
+	public static void main (String[] args){
+		System.out.println("Selesai Belajar Pemrograman Java");
+	}
+}
